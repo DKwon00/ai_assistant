3e5903c0839040e11175e0d25523df6ad8d411bf
dkwon00 <kwondevin@gmail.com>
2024-02-19 16:15:08 -0800

    connected to database, saves chat history

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,6 +3,7 @@ class ChatsController < ApplicationController
     def reply
         #potentially find a better way to authenticate requests
         if (user_signed_in?)
+
             client = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
             response = client.chat(
                 parameters: {
@@ -11,6 +12,9 @@ class ChatsController < ApplicationController
                     temperature: 0.7,
                 }
             )
+
+            current_user.q_and_a << [params["message"], response.dig("choices", 0, "message", "content")]
+            current_user.save
             render json: response.dig("choices", 0, "message", "content").to_json
         end
     end
