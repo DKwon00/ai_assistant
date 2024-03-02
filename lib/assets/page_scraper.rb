13e16fcc58c428b9a9c7355688c0c5a8a4c55595
dkwon00 <kwondevin@gmail.com>
2024-03-01 22:07:11 -0800

    scrapper grabs additional links

--- lib/assets/page_scraper.rb
+++ lib/assets/page_scraper.rb
@@ -6,11 +6,16 @@ class GithubSpider < Tanakai::Base
     @engine = :selenium_chrome
     @start_urls = ["https://studentaid.gov/understand-aid/types/loans/subsidized-unsubsidized"]
     @config = {
-      user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+      user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+      before_request: {delay: 1..5}
     }
   
     def parse(response, url:, data: {})
-        puts response.xpath("//div//div[@class='col-lg-7']").text
+        puts response.xpath("//div[@class='col-lg-7']").text
+
+        response.xpath("//div[@class='col-lg-6 pb-0 addLink']/div/a").each do |a|
+          puts a[:href]
+        end
     end
   end
   
