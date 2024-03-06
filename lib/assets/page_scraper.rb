eb1cd3c4079c951a39f9b7ec53e7ea40540c3d5d
dkwon00 <kwondevin@gmail.com>
2024-03-06 15:59:10 -0800

    changed engine

--- lib/assets/page_scraper.rb
+++ lib/assets/page_scraper.rb
@@ -3,7 +3,7 @@ require 'tanakai'
 
 class GithubSpider < Tanakai::Base
     @name = "github_spider"
-    @engine = :selenium_chrome
+    @engine = :cuprite
     @start_urls = ["https://studentaid.gov/understand-aid/types/loans/subsidized-unsubsidized"]
     @config = {
       user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
@@ -11,12 +11,14 @@ class GithubSpider < Tanakai::Base
     }
   
     def parse(response, url:, data: {})
-        puts response.xpath("//div[@class='col-lg-7']").text
-
-        response.xpath("//div[@class='col-lg-6 pb-0 addLink']/div/a").each do |a|
-          puts a[:href]
-        end
+      #puts response.xpath("//div[@class='col-lg-7']").text
+      HTTParty.post('http://127.0.0.1.com/page', body: JSON.generate({ foo: 'bar' }), headers: { 'Content-Type' => 'application/json' })
+      puts url
+      response.xpath("//div[@class='col-lg-6 pb-0 addLink']/div/a").each do |a|
+        next unless unique?(:fafsa_url, absolute_url(a[:href], base: "https://studentaid.gov"))
+        request_to :parse, url: absolute_url(a[:href], base: "https://studentaid.gov")
+      end
     end
   end
-  
+
   GithubSpider.crawl!
