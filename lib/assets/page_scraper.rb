3811608837c6406187aab8f75983f5c6e76c9573
dkwon00 <kwondevin@gmail.com>
2024-03-06 19:23:04 -0800

    created web crawler for fafsa with recursion to additional links

--- lib/assets/page_scraper.rb
+++ lib/assets/page_scraper.rb
@@ -1,19 +1,23 @@
 require 'tanakai'
-
+require 'httparty'
 
 class GithubSpider < Tanakai::Base
     @name = "github_spider"
     @engine = :cuprite
-    @start_urls = ["https://studentaid.gov/understand-aid/types/loans/subsidized-unsubsidized"]
+    @start_urls = ["https://studentaid.gov/apply-for-aid/fafsa/filling-out"]
     @config = {
       user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
       before_request: {delay: 1..5}
     }
   
     def parse(response, url:, data: {})
-      #puts response.xpath("//div[@class='col-lg-7']").text
-      HTTParty.post('http://127.0.0.1.com/page', body: JSON.generate({ foo: 'bar' }), headers: { 'Content-Type' => 'application/json' })
-      puts url
+      unless response.xpath("//div[@class='col-lg-7']").text.empty?
+        HTTParty.post('http://127.0.0.1:3000/page', 
+                        body: JSON.generate({:data => response.xpath("//div[@class='col-lg-7']").text,
+                                            :page_url => url}), 
+                        headers: { 'Content-Type' => 'application/json' })
+      end
+
       response.xpath("//div[@class='col-lg-6 pb-0 addLink']/div/a").each do |a|
         next unless unique?(:fafsa_url, absolute_url(a[:href], base: "https://studentaid.gov"))
         request_to :parse, url: absolute_url(a[:href], base: "https://studentaid.gov")
