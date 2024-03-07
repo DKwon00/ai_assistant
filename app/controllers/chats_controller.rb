d0c1372cc7a545e988163d24b8b3f2a0f05205f4
DKwon00 <130162993+DKwon00@users.noreply.github.com>
2024-03-07 12:50:06 -0800

    refactored code and added some comments

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,29 +1,35 @@
 require "openai"
 class ChatsController < ApplicationController
-
     def reply
         #potentially find a better way to authenticate requests
         if (user_signed_in?)
-            response = message_to_chat(<<~CONTENT)
+            response = message_to_ai(<<~CONTENT)
                 Answer the question based on the context below, and
                 if the question can't be answered based on the context,
                 say \"I don't know\".
         
                 Context:
-                #{context}
+                #{get_context}
         
                 ---
         
                 Question: #{params["message"]}
             CONTENT
-            
+
+            #save the chats to database and send back AIs response to webpage
             current_user.q_and_a << [params["message"], response]
             current_user.save
             render json: response.to_json
         end
     end
+    
+    private
+    
+    def client
+        @client = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
+    end
 
-    def message_to_chat(message_content)
+    def message_to_ai(message_content)
         response = client.chat(
             parameters: {
                 model: "gpt-3.5-turbo-0125",
@@ -34,28 +40,23 @@ class ChatsController < ApplicationController
         return response.dig("choices", 0, "message", "content")
     end
     
-    def context
-        question_embedding = embedding_for(params["message"])
+    def get_context
+        question_embedding = generate_embedding(params["message"])
         nearest_items = Page.nearest_neighbors(
             :embedding, question_embedding,
             distance: "euclidean"
         )
 
-        context = nearest_items.first.text
+        get_context = nearest_items.first.text
     end
 
-    def embedding_for(text)
+    def generate_embedding(text)
         response = client.embeddings(
             parameters: {
                 model: 'text-embedding-3-small',
                 input: text
             }
         )
-        response.dig('data', 0, 'embedding')
+        return response.dig('data', 0, 'embedding')
     end
-
-    def client
-        @client = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
-    end
-
 end
