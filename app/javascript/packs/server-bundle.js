31bbfa978b1ead142d427fc1866d5118299e7526
DKwon00 <130162993+DKwon00@users.noreply.github.com>
2024-03-08 14:12:59 -0800

    removed NavBarServer

--- app/javascript/packs/server-bundle.js
+++ app/javascript/packs/server-bundle.js
@@ -1,8 +1,6 @@
 import ReactOnRails from 'react-on-rails';
-import NavBarServer from '../components/NavBarServer';
 
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-    NavBarServer,
 });
