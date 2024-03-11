c920ae3cc4ca0dbbb5406607299f62808f08954f
DKwon00 <kwondevin@gmail.com>
2024-03-11 16:57:03 -0700

    commented out layout

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HelloWorldController < ApplicationController
-  layout "hello_world"
+  #layout "hello_world"
 
   def index
     @hello_world_props = { name: "Stranger" }
