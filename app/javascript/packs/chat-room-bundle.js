4fb41534de66f11b75be7d4967896178e5923c72
DKwon00 <kwondevin@gmail.com>
2024-03-11 17:48:14 -0700

    added navbar to chatroom

--- app/javascript/packs/chat-room-bundle.js
+++ app/javascript/packs/chat-room-bundle.js
@@ -1,7 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
 import ChatRoom from '../bundles/ChatRoom/components/ChatRoom';
+import NavBar from '../components/NavBar';
+
 
 ReactOnRails.register({
     ChatRoom,
+    NavBar,
 });
