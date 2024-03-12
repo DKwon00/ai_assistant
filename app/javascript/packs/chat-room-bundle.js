44070c9ab79bead2ac9cf7373330f38f8d214bb0
DKwon00 <kwondevin@gmail.com>
2024-03-12 14:17:59 -0700

    added css

--- app/javascript/packs/chat-room-bundle.js
+++ app/javascript/packs/chat-room-bundle.js
@@ -3,7 +3,6 @@ import ReactOnRails from 'react-on-rails';
 import ChatRoom from '../bundles/ChatRoom/components/ChatRoom';
 import NavBar from '../bundles/NavBar/components/NavBar';
 
-
 ReactOnRails.register({
     ChatRoom,
     NavBar,
