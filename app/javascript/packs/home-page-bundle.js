b0fa14b7e90e96bbb24e0cd4e87cf3de4cc4fdbc
DKwon00 <130162993+DKwon00@users.noreply.github.com>
2024-03-13 13:35:20 -0700

    Delete app/javascript/packs/home-page-bundle.js

--- app/javascript/packs/home-page-bundle.js
+++
@@ -1,9 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-
-import HomePage from '../bundles/HomePage/components/HomePage';
-import NavBar from '../bundles/NavBar/components/NavBar';
-
-ReactOnRails.register({
-    HomePage,
-    NavBar,
-});
