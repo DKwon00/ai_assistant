d07b412449cbb5cb326ea2fbbd1910caad630305
DKwon00 <130162993+DKwon00@users.noreply.github.com>
2024-03-14 08:18:21 -0700

    removed framer element

--- app/javascript/bundles/HomePage/components/HomePage.jsx
+++ app/javascript/bundles/HomePage/components/HomePage.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { motion } from "framer-motion";
 
 const HomePage = () => {
   return (
-    <motion.div
-    initial={{ opacity: 0, scale: 0.5 }}
-    animate={{ opacity: 1, scale: 1 }}
-    transition={{ duration: 0.5 }}
-    />
+    <div>Please update the homepage!</div>
   )
 }
 
