543f554c09414cc32678cb6ab6d7665936b63c1f
DKwon00 <130162993+DKwon00@users.noreply.github.com>
2024-03-14 08:14:44 -0700

    added basic framer element

--- app/javascript/bundles/HomePage/components/HomePage.jsx
+++ app/javascript/bundles/HomePage/components/HomePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { motion } from "framer-motion";
 
 const HomePage = () => {
   return (
-    <div>Please update the homepage!</div>
+    <motion.div
+    initial={{ opacity: 0, scale: 0.5 }}
+    animate={{ opacity: 1, scale: 1 }}
+    transition={{ duration: 0.5 }}
+    />
   )
 }
 
