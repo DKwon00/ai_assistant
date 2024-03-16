b2888135d214741ed6b80942542665d0ba59033d
DKwon00 <kwondevin@gmail.com>
2024-03-15 22:01:43 -0700

    added web crawler

--- lib/assets/page_scraper.rb
+++ lib/assets/page_scraper.rb
@@ -1,10 +1,10 @@
-#require 'tanakai'
+require 'tanakai'
 require 'httparty'
 
 class WebSpider < Tanakai::Base
-    @home_url = "https://studentaid.gov"
+    @home_url = "https://minecraft.fandom.com/wiki/Minecraft_Wiki"
     @name = "web_spider"
-    @engine = :cuprite
+    @engine = :mechanize
     @start_urls = [@home_url]
     @config = {
       user_agent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
@@ -13,20 +13,20 @@ class WebSpider < Tanakai::Base
   
     def parse(response, url:, data: {})
       #grab the contents of the page
-      page_text = response.xpath("//div[@class='col-lg-7']").text
-        
-      unless page_text.empty?
-        HTTParty.post('http://127.0.0.1:3000/page', 
-                        body: JSON.generate({:data => page_text, :page_url => url}), 
-                        headers: { 'Content-Type' => 'application/json' })
-      end
-      #recurse through any additional links found on the webpage
-      response.xpath("//a[@href]").each do |a|
-        next_url = absolute_url(a[:href], base: @home_url)
+      page_text = response.xpath("//div[@class='mw-parser-output']").text
+      puts page_text
+      # unless page_text.empty?
+      #   HTTParty.post('http://127.0.0.1:3000/page', 
+      #                   body: JSON.generate({:data => page_text, :page_url => url}), 
+      #                   headers: { 'Content-Type' => 'application/json' })
+      # end
+      # #recurse through any additional links found on the webpage
+      # response.xpath("//a[@href]").each do |a|
+      #   next_url = absolute_url(a[:href], base: @home_url)
           
-        next unless unique?(:fafsa_url, next_url)
-        request_to :parse, url: next_url
-      end
+      #   next unless unique?(:fafsa_url, next_url)
+      #   request_to :parse, url: next_url
+      # end
     end
   end
 
