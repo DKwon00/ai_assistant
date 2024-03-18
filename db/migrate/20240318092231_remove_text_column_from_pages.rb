b4140673bcd9b699a51a38bf4105cce76ad68f0a
DKwon00 <kwondevin@gmail.com>
2024-03-18 16:34:51 -0700

    removed migrations, resetting db

--- db/migrate/20240318092231_remove_text_column_from_pages.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTextColumnFromPages < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :pages, :text
-  end
-end
--- db/migrate/20240318111217_add_text_to_page.rb
+++
@@ -1,5 +0,0 @@
-class AddTextToPage < ActiveRecord::Migration[7.1]
-  def change
-    add_column :pages, :text, :string
-  end
-end
