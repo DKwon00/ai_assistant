23a0a5fbb2eb0108a4dd9077d831a4669826a134
DKwon00 <kwondevin@gmail.com>
2024-03-27 20:58:46 -0700

    changed routing for demo purposes

--- app/javascript/bundles/Message/components/Message.jsx
+++ app/javascript/bundles/Message/components/Message.jsx
@@ -1,5 +1,6 @@
 import "./Message.css";
 import { motion } from "framer-motion";
+import icon from "./minecraftIcon.png"
 import React from 'react';
 
 export default function Message(props) {
@@ -7,14 +8,15 @@ export default function Message(props) {
     return (
       <motion.div
         className="message"
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
+        initial={{ y: [0, 0] }}
+        animate={{ y: [50, 0] }}
         transition={{
           duration: 0.8,
           ease: [0, 0.71, 0.2, 1.01],
         }}
       >
-        {props.message}
+        <img src={icon} />
+        <div className="text">{props.message}</div>
       </motion.div>
     );
   }
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "/", to: "home_page#index"
+  # get "/", to: "home_page#index"
   get "/send", to: 'chat_room#reply'
-  get "/chat", to: 'chat_room#index'
+  get "/", to: 'chat_room#index'
   post "/page", to: 'pages#create'
   # Defines the root path route ("/")
   # root "posts#index"
