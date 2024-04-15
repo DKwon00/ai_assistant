22ebce90b3db9f743fd1009719a84bee0e377391
DKwon00 <kwondevin@gmail.com>
2024-04-15 15:29:28 -0700

    added homepage to website

--- app/javascript/bundles/HomePage/components/HomePage.jsx
+++ app/javascript/bundles/HomePage/components/HomePage.jsx
@@ -1,9 +1,111 @@
-import React from 'react';
+import "./HomePage.css";
+import { motion } from "framer-motion";
+import React from "react";
 
-const HomePage = () => {
+export default function HomePage() {
+  document.body.style = "background: linear-gradient(140deg, rgba(0,130,147,1) 0%, rgba(10,10,10,1) 34%, rgba(10,10,10,1) 64%, rgba(57,57,255,1) 99%);";
+  const title = "Juni-AI";
+
+  const container = {
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: 0.1, delayChildren: 0.4 * i },
+    }),
+  };
+
+  const child = {
+    visible: {
+      opacity: 1,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+    hidden: {
+      opacity: 0,
+    },
+  };
+
   return (
-    <div>Please update the homepage!</div>
-  )
+    <div className="homePage">
+      <div className="logo">
+        <div className="logoEye">
+          <motion.div
+            className="eye"
+            initial={{ opacity: 1, scale: 0.5 }}
+            animate={{
+              opacity: 1,
+              scale: 1
+            }}
+            transition={{
+              duration: 1.5,
+              delay: 0.1,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+          ></motion.div>
+          <motion.div
+            className="eyelid"
+            initial={{ y: 0, opacity: 1 }}
+            animate={{ y: -200, opacity: 0 }}
+            transition={{
+              duration: 0.5,
+              delay: 0.1,
+              ease: [1, 0.71, 0.4, 1.01],
+            }}
+          ></motion.div>
+          <motion.div
+            className="eyeMouth"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.3,
+              ease: [0, 0.71, 0.2, 1.01],
+              scale: {
+                type: "spring",
+                damping: 10,
+                stiffness: 100,
+                restDelta: 0.001,
+              },
+            }}
+          ></motion.div>
+        </div>
+        <motion.div
+          className="logoText"
+          variants={container}
+          initial="hidden"
+          animate="visible"
+        >
+          {title.split("").map((letter, index) => (
+            <motion.p className="typing" variants={child} key={index}>
+              {letter}
+            </motion.p>
+          ))}
+          <motion.p
+            className="typing"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              delay: 1.5,
+              duration: 0.5,
+              repeat: Infinity,
+              repeatDelay: 0.75,
+            }}
+          >
+            _
+          </motion.p>
+        </motion.div>
+      </div>
+      <motion.a 
+        href="google.com"
+        className="link" 
+        initial={{ opacity: 0, scale: 1 }}
+        animate={{ opacity: 1, scale: 1 }}             
+        transition={{ delay: 1.75 }} 
+        re
+        >
+          Chat Now
+      </motion.a>
+    </div>
+  );
 }
-
-export default HomePage;
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "/", to: "home_page#index"
+  get "/", to: "home_page#index"
   get "/send", to: 'chat_room#reply'
-  get "/", to: 'chat_room#index'
+  get "/chat", to: 'chat_room#index'
   get "/get", to: 'chat_room#get_chat_history' 
   post "/page", to: 'pages#create'
   get "/destroy", to: 'chat_room#destroy'
