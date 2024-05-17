30ee5952afcb8614f047975baf8e2c920bf3d76a
DKwon00 <130162993+DKwon00@users.noreply.github.com>
2024-05-17 09:00:19 -0700

    created a loading animation component

---
+++ app/javascript/bundles/Loading/components/Loading.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const loadingContainer = {
+  width: "2rem",
+  height: "2rem",
+  display: "flex",
+  justifyContent: "space-around",
+};
+
+const loadingCircle = {
+  display: "block",
+  width: "0.5rem",
+  height: "0.5rem",
+  backgroundColor: "#cccccc",
+  borderRadius: "0.25rem",
+};
+
+const loadingContainerVariants = {
+  start: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+  end: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const loadingCircleVariants = {
+  start: {
+    opacity: 0.5,
+    y: "100%",
+  },
+  end: {
+    opacity: 1,
+    y: "130%",
+  },
+};
+
+const loadingCircleTransition = {
+  duration: 0.5,
+  repeatType: "mirror",
+  repeat: Infinity,
+  ease: "easeInOut",
+};
+
+export default function ThreeDotsWave() {
+  return (
+    <motion.div
+      style={loadingContainer}
+      variants={loadingContainerVariants}
+      initial="start"
+      animate="end"
+    >
+      <motion.span
+        style={loadingCircle}
+        variants={loadingCircleVariants}
+        transition={loadingCircleTransition}
+      />
+      <motion.span
+        style={loadingCircle}
+        variants={loadingCircleVariants}
+        transition={loadingCircleTransition}
+      />
+      <motion.span
+        style={loadingCircle}
+        variants={loadingCircleVariants}
+        transition={loadingCircleTransition}
+      />
+    </motion.div>
+  );
+}
